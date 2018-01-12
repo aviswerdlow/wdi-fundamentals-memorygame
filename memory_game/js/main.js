3f89a6ff629d9ac03fe6a74a671a06c81727ec3b
Swerdlow, Avi <Avi.Swerdlow@disney.com>
2018-01-11 16:29:09 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 
 // Array to store cards
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+];
 
 // Array to hold the cards that are in play
 var cardsInPlay = [];
@@ -19,9 +40,11 @@ var checkForMatch = function() {
 
 var flipCard = function(cardId) {
 	//let user know what card has been flipped
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	//add flipped card to cards played
-	cardsInPlay.push(cards[cardId]);	
+	cardsInPlay.push(cards[cardId].rank);	
 	checkForMatch();
 }
 
